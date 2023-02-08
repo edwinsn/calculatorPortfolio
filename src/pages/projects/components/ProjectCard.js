36b1d0693782c1dc970a3ca1d758397668c307ef
edwinsn <edwin@sprwt.io>
2023-02-08 04:59:14 -0500

    Improved the products card expand description transition

--- src/pages/projects/components/ProjectCard.js
+++ src/pages/projects/components/ProjectCard.js
@@ -1,33 +1,25 @@
 import React, { useState } from 'react'
 import './projectCard.css'
-import githubLinkIcon from '../../../assets/images/githubprojectLink.svg'
-import linkIcon from '../../../assets/images/goto.svg'
 import { Carousel } from 'react-responsive-carousel';
 import arrowIcon from '../../../assets/images/deployIcon.svg'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import useWindowDimensions from '../../components/useWindowDimensions';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+
+import ProjectDescription from './ProjectDescription';
 
 export default function ProjectCard({
   imgs,
+  backgroundColor,
+  className,
   tittle,
   icon,
   description,
   shortDescription,
   href,
   githubLink,
-  backgroundColor,
-  className
 }) {
 
 
-  const { width } = useWindowDimensions()
-
-  const issmallView = width < 950
-
-  const [on, setOn] = useState(false);
-
+  const [on, setOn] = useState(true);
 
   const projectImgs = imgs?.map((img, index) => {
     return <img key={`proyectImg-${index}`} src={img} alt='' />
@@ -47,50 +39,30 @@ export default function ProjectCard({
         style={{ backgroundColor }}
       >
         <input id="menu" type="checkbox" checked={on} />
-        
+
         <Carousel
           emulateTouch
           infiniteLoop showThumbs={false}
           useKeyboardArrows
           showStatus={false}
           autoPlay={false}
-          className='project-carousel nonselectable'
+          className='project-carousel nonselectable cursor-pointer'
           renderArrowPrev={(handler) => carouselArrow(handler, 'left')}
           renderArrowNext={(handler) => carouselArrow(handler, 'right')}
           onClickItem={() => setOn(!on)}
         >
           {projectImgs}
         </Carousel>
-
-        <div className="project-body column centered">
-
-          <FontAwesomeIcon icon={faAngleUp} className="iconUp" onClick={() => setOn(!on)} />
-
-          <div className='tittle-section centered'>
-            <span className='project-tittle'>
-              {tittle}
-
-            </span>
-            <img src={icon} alt='' className='project-icon' />
-          </div>
-          {!issmallView && <p className='text-centered project-description' >{description}</p>}
-          {issmallView && <p className='text-centered project-description' >{shortDescription}</p>}
-          <div className='project-actions flex'>
-            <a href={href}
-              className='project-link-icon'
-              target="_blank"
-              rel="noreferrer">
-              <img src={linkIcon} alt='' />
-            </a>
-            <a href={githubLink}
-              className="project-github-link"
-              target="_blank"
-              rel="noreferrer">
-              <img src={githubLinkIcon} alt='' />
-            </a>
-          </div>
-
-        </div>
+        <ProjectDescription
+          open={on}
+          setOpen={setOn}
+          tittle={tittle}
+          icon={icon}
+          description={description}
+          shortDescription={shortDescription}
+          href={href}
+          githubLink={githubLink}
+        />
       </div>
     </div>
   )
---
+++ src/pages/projects/components/ProjectDescription.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesUp } from '@cseitz/icons-duotone';
+import githubLinkIcon from '../../../assets/images/githubprojectLink.svg'
+import linkIcon from '../../../assets/images/goto.svg'
+import useWindowDimensions from '../../components/useWindowDimensions';
+
+
+export default function ProjectDescription({
+    open,
+    setOpen,
+    tittle,
+    icon,
+    description,
+    shortDescription,
+    href,
+    githubLink,
+}) {
+
+    const { width } = useWindowDimensions()
+    const issmallView = width < 950;
+
+    return (
+        <div className="project-body column centered">
+            <FontAwesomeIcon
+                icon={faAnglesUp}
+                className={`deploy-project-description cursor-pointer ${!open ? 'down' : ''}`}
+                onClick={() => setOpen(prev => !prev)}
+            />
+            <div className='tittle-section centered'>
+                <span className='project-tittle'>
+                    {tittle}
+                </span>
+                <img src={icon} alt='' className='project-icon' />
+            </div>
+            {
+                !open &&
+                (
+                    <>
+                        {!issmallView && <p className='text-centered project-description' >{description}</p>}
+                        {issmallView && <p className='text-centered project-description' >{shortDescription}</p>}
+                        <div className='project-actions flex'>
+                            <a href={href}
+                                className='project-link-icon'
+                                target="_blank"
+                                rel="noreferrer">
+                                <img src={linkIcon} alt='' />
+                            </a>
+                            <a href={githubLink}
+                                className="project-github-link"
+                                target="_blank"
+                                rel="noreferrer">
+                                <img src={githubLinkIcon} alt='' />
+                            </a>
+                        </div>
+                    </>
+                )
+            }
+
+        </div>
+    )
+}
