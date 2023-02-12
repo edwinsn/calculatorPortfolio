f0fd37cc1a4a9252fa494f1a19c5cdedbdccddac
Sofia Tovar <d015yyqb3@mozmail.com>
2023-02-12 16:46:42 -0500

    Se le agrego la animacion a la seccion de skills

--- src/pages/sills/Skills.js
+++ src/pages/sills/Skills.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import backendIcon from '../../assets/images/skills/backendIcon.svg'
 import frontendIcon from '../../assets/images/skills/frontendIcon.svg'
 import designIcon from '../../assets/images/skills/designIcon.svg'
@@ -23,131 +23,273 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css';
 
 
-
 export default function Skills() {
 
 
+
     return (
-        <ShowUp className='w-90 flex-column centered full-screen'>
+
+        <div className='main flex-column centered w-90 full-screen' >
             <h2 id='skills-title' className='text-white'>
                 Skills
             </h2>
-            <Tabs id='skills' className='w-100'>
-                {/** Content */}
-                <TabPanel >
-                    <h2 className='subtitle'>
-                        Frontend
-                    </h2>
-                    <div>
-                        <FontAwesomeIcon icon={faReact} className="iconSkill colorReact" title='react' />
-                        <FontAwesomeIcon icon={faHtml5} className="iconSkill orange" title='html' />
-                        <FontAwesomeIcon icon={faCss3Alt} className="iconSkill colorCss" title='css' />
-                        <img src={reduxIcon} alt="" title='redux' />
-                        <img src={reactQueryIcon} alt="" title='react query' />
-                    </div>
-                    <p>
-                        React, the perfect ingredient for building amazing applications.
-                        I have a deep love for the reactive philosophy and
-                        its focus on state-driven components and dynamic
-                        code updates. This allows for an intuitive and
-                        user-friendly experience. My technical skills include
-                        React, HTML, CSS, Redux, and React Query. With
-                        these tools in my belt, I'm ready to take on any project
-                        and make it a React-tastic success!"
-                    </p>
-                </TabPanel>
-                <TabPanel>
-                    <h2 className='subtitle'>
-                        Backend
-                    </h2>
-                    <div>
-                        <FontAwesomeIcon icon={faNodeJs} className="iconSkill colorNode" title='nodejs' />
-                        <img src={sqlIcon} alt="" title='sql' />
-                        <FontAwesomeIcon icon={faAws} className="iconSkill colorAws" title='aws' />
-                        <img src={mongoIcon} alt="" title='mongodb' />
-                        <img src={firebaseIcon} alt="" title='firebase' />
-                    </div>
 
-                    <p>
-                        Say hello to seamless backend systems! I'm proficient in using
-                        Node.js, MongoDB, SQL, and AWS to create efficient and
-                        reliable solutions. My in-depth knowledge of these
-                        technologies enables me to design and implement secure,
-                        scalable backend systems that can handle the demands of
-                        modern applications. So whether you need a system that's
-                        simple and straightforward or one that's complex and
-                        powerful, I've got you covered.
-                    </p>
-                </TabPanel>
+            <div id="skills" className='flex centered'>
 
-                <TabPanel>
-                    <h2 className='subtitle'>
-                        Design
-                    </h2>
-                    <div>
-                        <img src={figmaIcon} alt="" title='figma' />
-                        <img src={gimpIcon} alt="" title='GIMP' />
-                    </div>
-                    <p>
-                        With Figma and GIMP in my toolbox, I bring a playful
-                        yet professional touch to visual design. Whether it's
-                        a logo, website, or any other design project, I have
-                        the skills to turn your ideas into eye-catching,
-                        user-friendly designs that stand out and make a lasting
-                        impression.
-                    </p>
-                </TabPanel>
-                <TabPanel>
-                    <h2 className='subtitle'>
-                        Others
-                    </h2>
-                    <div>
-                        <FontAwesomeIcon icon={faGitAlt} className="iconSkill orange" title='git' />
-                        <img src={jestIcon} alt="" title='jest' />
-                    </div>
-                    <p>
-                        Git and Jest? More like hit and the best! These tools
-                        are my trusty sidekicks in the wild world of web
-                        development. With Git, I can easily track my code
-                        and collaborate with my team. And with Jest, I can
-                        be confident that my code is not only shiny, but
-                        also functions like a well-oiled machine.
-                        Let's just say, I'm ready for any coding adventure
-                        that comes my way!
-                    </p>
-                </TabPanel>
+                <div className='flex-column centered' >
 
-                {/** Options */}
-                <TabList>
-                    <Tab>
+                    <input type="radio" name="same" id="frontend" className='display-none' checked={true} />
+                    <label for="frontend" >
                         <img src={frontendIcon} alt="" />
-                        <span>
+                    </label>
+                    <span>
+                        FrontEnd
+                    </span>
+
+                    <div>
+                        <h2 className='subtitle'>
                             Frontend
-                        </span>
-                    </Tab>
-                    <Tab>
+                        </h2>
+                        <div>
+                            <FontAwesomeIcon icon={faReact} className="iconSkill colorReact" title='react' />
+                            <FontAwesomeIcon icon={faHtml5} className="iconSkill orange" title='html' />
+                            <FontAwesomeIcon icon={faCss3Alt} className="iconSkill colorCss" title='css' />
+                            <img src={reduxIcon} alt="" title='redux' />
+                            <img src={reactQueryIcon} alt="" title='react query' />
+                        </div>
+                        <p>
+                            React, the perfect ingredient for building amazing applications.
+                            I have a deep love for the reactive philosophy and
+                            its focus on state-driven components and dynamic
+                            code updates. This allows for an intuitive and
+                            user-friendly experience. My technical skills include
+                            React, HTML, CSS, Redux, and React Query. With
+                            these tools in my belt, I'm ready to take on any project
+                            and make it a React-tastic success!"
+                        </p>
+
+                    </div>
+                </div>
+
+                <div className='flex-column centered'>
+
+                    <input type="radio" name="same" id="backend" className='display-none' checked={true} />
+                    <label for="backend" >
                         <img src={backendIcon} alt="" />
-                        <span>
+                    </label>
+                    <span>
+                        Backend
+                    </span>
+
+                    <div>
+                        <h2 className='subtitle'>
                             Backend
-                        </span>
-                    </Tab>
-                    <Tab>
+                        </h2>
+                        <div>
+                            <FontAwesomeIcon icon={faNodeJs} className="iconSkill colorNode" title='nodejs' />
+                            <img src={sqlIcon} alt="" title='sql' />
+                            <FontAwesomeIcon icon={faAws} className="iconSkill colorAws" title='aws' />
+                            <img src={mongoIcon} alt="" title='mongodb' />
+                            <img src={firebaseIcon} alt="" title='firebase' />
+                        </div>
+
+                        <p>
+                            Say hello to seamless backend systems! I'm proficient in using
+                            Node.js, MongoDB, SQL, and AWS to create efficient and
+                            reliable solutions. My in-depth knowledge of these
+                            technologies enables me to design and implement secure,
+                            scalable backend systems that can handle the demands of
+                            modern applications. So whether you need a system that's
+                            simple and straightforward or one that's complex and
+                            powerful, I've got you covered.
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex-column centered'>
+
+                    <input type="radio" name="same" id="design" className='display-none'  checked={true}/>
+                    <label for="design" >
                         <img src={designIcon} alt="" />
-                        <span>
+                    </label>
+                    <span>
+                        Design
+                    </span>
+
+                    <div>
+                        <h2 className='subtitle'>
                             Design
-                        </span>
-                    </Tab>
-                    <Tab>
+                        </h2>
+                        <div>
+                            <img src={figmaIcon} alt="" title='figma' />
+                            <img src={gimpIcon} alt="" title='GIMP' />
+                        </div>
+                        <p>
+                            With Figma and GIMP in my toolbox, I bring a playful
+                            yet professional touch to visual design. Whether it's
+                            a logo, website, or any other design project, I have
+                            the skills to turn your ideas into eye-catching,
+                            user-friendly designs that stand out and make a lasting
+                            impression.
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex-column centered'>
+
+                    <input type="radio" name="same" id="others" className='display-none' checked={true} />
+                    <label for="others">
                         <img src={othersIcon} alt="" />
-                        <span>
+                    </label>
+                    <span>
+                        Others
+                    </span>
+
+                    <div>
+                        <h2 className='subtitle'>
                             Others
-                        </span>
-                    </Tab>
+                        </h2>
+                        <div>
+                            <FontAwesomeIcon icon={faGitAlt} className="iconSkill orange" title='git' />
+                            <img src={jestIcon} alt="" title='jest' />
+                        </div>
+                        <p>
+                            Git and Jest? More like hit and the best! These tools
+                            are my trusty sidekicks in the wild world of web
+                            development. With Git, I can easily track my code
+                            and collaborate with my team. And with Jest, I can
+                            be confident that my code is not only shiny, but
+                            also functions like a well-oiled machine.
+                            Let's just say, I'm ready for any coding adventure
+                            that comes my way!
+                        </p>
+                    </div>
 
-                </TabList>
+                </div>
+            </div>
 
-            </Tabs>
-        </ShowUp>
+        </div>
+
+        /*<ShowUp className='w-90 flex-column centered full-screen'>
+                <h2 id='skills-title' className='text-white'>
+                    Skills
+                </h2>
+                <Tabs id='skills' className='w-100'>
+
+                    <TabPanel >
+                        <h2 className='subtitle'>
+                            Frontend
+                        </h2>
+                        <div>
+                            <FontAwesomeIcon icon={faReact} className="iconSkill colorReact" title='react' />
+                            <FontAwesomeIcon icon={faHtml5} className="iconSkill orange" title='html' />
+                            <FontAwesomeIcon icon={faCss3Alt} className="iconSkill colorCss" title='css' />
+                            <img src={reduxIcon} alt="" title='redux' />
+                            <img src={reactQueryIcon} alt="" title='react query' />
+                        </div>
+                        <p>
+                            React, the perfect ingredient for building amazing applications.
+                            I have a deep love for the reactive philosophy and
+                            its focus on state-driven components and dynamic
+                            code updates. This allows for an intuitive and
+                            user-friendly experience. My technical skills include
+                            React, HTML, CSS, Redux, and React Query. With
+                            these tools in my belt, I'm ready to take on any project
+                            and make it a React-tastic success!"
+                        </p>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2 className='subtitle'>
+                            Backend
+                        </h2>
+                        <div>
+                            <FontAwesomeIcon icon={faNodeJs} className="iconSkill colorNode" title='nodejs' />
+                            <img src={sqlIcon} alt="" title='sql' />
+                            <FontAwesomeIcon icon={faAws} className="iconSkill colorAws" title='aws' />
+                            <img src={mongoIcon} alt="" title='mongodb' />
+                            <img src={firebaseIcon} alt="" title='firebase' />
+                        </div>
+
+                        <p>
+                            Say hello to seamless backend systems! I'm proficient in using
+                            Node.js, MongoDB, SQL, and AWS to create efficient and
+                            reliable solutions. My in-depth knowledge of these
+                            technologies enables me to design and implement secure,
+                            scalable backend systems that can handle the demands of
+                            modern applications. So whether you need a system that's
+                            simple and straightforward or one that's complex and
+                            powerful, I've got you covered.
+                        </p>
+                    </TabPanel>
+
+                    <TabPanel>
+                        <h2 className='subtitle'>
+                            Design
+                        </h2>
+                        <div>
+                            <img src={figmaIcon} alt="" title='figma' />
+                            <img src={gimpIcon} alt="" title='GIMP' />
+                        </div>
+                        <p>
+                            With Figma and GIMP in my toolbox, I bring a playful
+                            yet professional touch to visual design. Whether it's
+                            a logo, website, or any other design project, I have
+                            the skills to turn your ideas into eye-catching,
+                            user-friendly designs that stand out and make a lasting
+                            impression.
+                        </p>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2 className='subtitle'>
+                            Others
+                        </h2>
+                        <div>
+                            <FontAwesomeIcon icon={faGitAlt} className="iconSkill orange" title='git' />
+                            <img src={jestIcon} alt="" title='jest' />
+                        </div>
+                        <p>
+                            Git and Jest? More like hit and the best! These tools
+                            are my trusty sidekicks in the wild world of web
+                            development. With Git, I can easily track my code
+                            and collaborate with my team. And with Jest, I can
+                            be confident that my code is not only shiny, but
+                            also functions like a well-oiled machine.
+                            Let's just say, I'm ready for any coding adventure
+                            that comes my way!
+                        </p>
+                    </TabPanel>
+
+                    { }
+                    <TabList>
+                        <Tab>
+                            <img src={frontendIcon} alt="" />
+                            <span>
+                                Frontend
+                            </span>
+                        </Tab>
+                        <Tab>
+                            <img src={backendIcon} alt="" />
+                            <span>
+                                Backend
+                            </span>
+                        </Tab>
+                        <Tab>
+                            <img src={designIcon} alt="" />
+                            <span>
+                                Design
+                            </span>
+                        </Tab>
+                        <Tab>
+                            <img src={othersIcon} alt="" />
+                            <span>
+                                Others
+                            </span>
+                        </Tab>
+
+                    </TabList>
+
+                </Tabs>
+            </ShowUp>*/
     )
 
     /*return (
@@ -162,5 +304,5 @@ export default function Skills() {
                     <DropDownList title="Others" icon={othersIcon} items={["Git", "Jest", "JsDoc", "Firebase"]} />
                 </div>
             </div>
-    ) */
+            ) */
 }   
